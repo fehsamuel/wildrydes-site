3c4b91531718fe308a9d94820ab4210eb67fe759
fehsamuel <fiverrgrace1@gmail.com>
2024-12-24 14:28:37 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // us-east-1_e7W3Orftc
-        userPoolClientId: '', 7fu92fvqiiiu10t6o5metho7rk
-        region: '' // us-east-1
+        userPoolId: 'us-east-1_TMscWPA4g',
+        userPoolClientId: '5re0qfdukjfloaj1p07o345icu',
+        region: 'us-east-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
